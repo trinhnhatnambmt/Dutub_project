fd5419ff5d9346d84058840e246cf6923a010786
Huy Trịnh <huytnse170480@fpt.edu.vn>
2024-08-02 14:26:25 +0700

    Doing intro

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 import logo from "./img/Logo_DuTub.svg";
-
-export { logo };
+import logo2 from "./img/Logo_DuTub_2.svg";
+import introVideo from "./video/introVideo_page.mp4";
+export { logo, logo2, introVideo };
